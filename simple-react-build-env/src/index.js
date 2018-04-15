bf1be4fe13886f265613e19d851e6bd9aadea349
Khoi Le <minarek@outlook.com>
2018-04-15 13:16:45 +0200

    Updated index.js

--- simple-react-build-env/src/index.js
+++ simple-react-build-env/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <div>You are wellcome to React and Webpack Dev Server! Auto refresh on Windows, MacOS and Debian. It works fine now!</div>,
+    <div>You are wellcome to React and Webpack Dev Server! Auto refresh on Windows, MacOS and Debian.</div>,
     document.getElementById('dspMessage')
 );
