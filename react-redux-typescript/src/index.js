1841047e6fd96fb3695123bc77949b7d3d28d09f
Khoi Le <minarek@outlook.com>
2018-07-24 10:15:39 +0200

    Updated codes

--- react-redux-typescript/src/index.js
+++ react-redux-typescript/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import css from './styles.css';
+import styles from './styles.css';
 
 ReactDOM.render(
     <div className='welcome'>Welcome to React and Webpack Dev Server! Auto refresh on Windows, MacOS and Debian. CSS included in HTML page.</div>,
