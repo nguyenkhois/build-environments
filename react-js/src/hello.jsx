8651e1243a14d9e1f01e10c8c38dc2bfcc2e03e3
Khoi Le <minarek@outlook.com>
2018-08-07 13:29:32 +0200

    Improved code

---
+++ react-js/src/hello.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class Hello extends Component{
+    //Do stuff
+    
+    //Render
+    render(){
+        return(
+            <div className="welcome">
+                Welcome from React and Pure JavaScript!
+            </div>
+        );
+    }
+};
